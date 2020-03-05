d6d647e2d4dd47527dd48c3740f119e04542e25d
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-03-05 16:32:13 +0300

    service was used instead repository

--- src/main/java/com/asellion/assignment/bootstrap/Bootstrap.java
+++ src/main/java/com/asellion/assignment/bootstrap/Bootstrap.java
@@ -22,47 +22,48 @@ public class Bootstrap implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Product product1 = Product.builder()
                 .name("Phone")
-                .currentPrice(new BigDecimal(14.20))
+                .currentPrice(new BigDecimal(12.20))
                 .lastUpdate(new Date())
                 .build();
 
         Product product2 = Product.builder()
                 .name("Book")
-                .currentPrice(new BigDecimal(15.50))
+                .currentPrice(new BigDecimal(16.50))
                 .lastUpdate(new Date())
                 .build();
 
         Product product3 = Product.builder()
                 .name("Chair")
-                .currentPrice(new BigDecimal(30.23))
+                .currentPrice(new BigDecimal(31.23))
                 .lastUpdate(new Date())
                 .build();
 
         Product product4 = Product.builder()
                 .name("Kite")
-                .currentPrice(new BigDecimal(20.20))
+                .currentPrice(new BigDecimal(22.20))
                 .lastUpdate(new Date())
                 .build();
 
         Product product5 = Product.builder()
                 .name("Table")
-                .currentPrice(new BigDecimal(30.20))
+                .currentPrice(new BigDecimal(31.20))
                 .lastUpdate(new Date())
                 .build();
 
         Product product6 = Product.builder()
                 .name("Kindle")
-                .currentPrice(new BigDecimal(30.23))
+                .currentPrice(new BigDecimal(32.23))
                 .lastUpdate(new Date())
                 .build();
 
+        productService.createProduct(product1);
+        productService.createProduct(product2);
+        productService.createProduct(product3);
+        productService.createProduct(product4);
+        productService.createProduct(product5);
+        productService.createProduct(product6);
 
-        productRepository.save(product1);
-        productRepository.save(product2);
-        productRepository.save(product3);
-        productRepository.save(product4);
-        productRepository.save(product5);
-        productRepository.save(product6);
+
 
 
         List<Product> productList = productService.getAllProducts();
